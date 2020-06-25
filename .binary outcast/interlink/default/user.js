83ddd8b6271b58bc292ba2c3063f1c10f0c21274
Dennis Lee <dennis@dennislee.xyz>
2020-06-24 22:49:30 -0700

    email client from mailspring to interlink/birdtray

---
+++ .binary outcast/interlink/default/user.js
@@ -0,0 +1,5 @@
+// user_pref("alerts.totalOpenTime", 3600000);
+user_pref("app.eula.accepted", true);
+// user_pref("mailnews.start_page.url", "about:config");
+user_pref("general.warnOnAboutConfig", false);
+user_pref("extensions.muttator.firsttime", false);
