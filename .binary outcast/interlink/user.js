05c038a20e55038ae8760ca287973c9186e8484b
Dennis Lee <dennis@dennislee.xyz>
2020-08-08 23:10:47 -0700

    some random other stuff

--- .binary outcast/interlink/user.js
+++ .binary outcast/interlink/user.js
@@ -1,26 +1,43 @@
-// user_pref("mail.folderpane.showColumns", true);
-// user_pref("mailnews.start_page.enabled", false);
 user_pref("app.eula.accepted", true);
 user_pref("app.update.enabled", false);
-user_pref("calendar.agendaListbox.soondays", 28);
-user_pref("calendar.integration.notify", false);
-user_pref("calendar.item.editInTab", true);
-user_pref("calendar.view.useSystemColors", true);
-user_pref("calendar.view.visiblehours", 24);
-user_pref("extensions.muttator.firsttime", false);
-user_pref("general.autoScroll", true);
 user_pref("general.warnOnAboutConfig", false);
+user_pref("extensions.muttator.firsttime", false);
+
 user_pref("layers.acceleration.enabled", true);
+
+user_pref("general.autoScroll", true);
+
+user_pref("network.http.altsvc.oe", true);
+user_pref("network.http.upgrade-insecure-requests", true);
+
+user_pref("network.cookie.cookieBehavior", 2);
+user_pref("places.history.enabled", false);
+
+// return_receipt
 user_pref("mail.incorporate.return_receipt", 1);
-user_pref("mail.pane_config.dynamic", 2);
 user_pref("mail.receipt.request_return_receipt_on", true);
-user_pref("mail.serverDefaultStoreContractID", "@mozilla.org/msgstore/maildirstore;1");
+
+// mark_message_read
+user_pref("mailnews.mark_message_read.delay", true);
+user_pref("mailnews.mark_message_read.delay.interval", 1);
+
+// spam
 user_pref("mail.spam.logging.enabled", true);
 user_pref("mail.spam.manualMark", true);
 user_pref("mail.spam.markAsReadOnSpam", true);
-user_pref("mailnews.mark_message_read.delay", true);
-user_pref("mailnews.mark_message_read.delay.interval", 1);
-user_pref("network.cookie.cookieBehavior", 2);
-user_pref("network.http.altsvc.oe", true);
-user_pref("network.http.upgrade-insecure-requests", true);
-user_pref("places.history.enabled", false);
+
+user_pref("calendar.agendaListbox.soondays", 28);
+user_pref("calendar.integration.notify", false);
+user_pref("calendar.item.editInTab", true);
+user_pref("calendar.view.visiblehours", 24);
+
+user_pref("mail.serverDefaultStoreContractID", "@mozilla.org/msgstore/maildirstore;1");
+
+// https://wiki.archlinux.org/index.php/Thunderbird#Theming_tweaks
+user_pref("browser.display.use_system_colors", true);
+user_pref("browser.display.document_color_use", 2);
+user_pref("calendar.view.useSystemColors", true);
+
+// user_pref("mail.folderpane.showColumns", true);
+// user_pref("mailnews.start_page.enabled", false);
+// user_pref("mail.pane_config.dynamic", 2);
